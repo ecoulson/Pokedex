c83474fda80d97e5921754e3e15934424231a77d
unholydeath785 <ivansonofcoul@gmail.com>
2018-04-17 15:16:44 -0700

    refactored code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,89 @@
-const SEARCHBAR = document.getElementsByClassName('pokedex__search')[0];
-const API_URL = 'https://pokeapi.co/api/v2/pokemon/';
+const API_URL = 'https://pokeapi.co/api/v2/';
 const RETURN_KEYCODE = 13;
 
-SEARCHBAR.addEventListener('keyup', function handleChange(event) {
-	let code = event.keyCode;
-	if (code == RETURN_KEYCODE) {
-		
-	} else {
-		// autofill results
+class App {
+	constructor() {
+		this.search = new Search('.pokedex__search-bar');
 	}
-});
+
+	start() {
+		this.initComponents();
+	}
+
+	initComponents() {
+		this.search.init();
+		console.log(this.search);
+	}
+}
+
+class Component {
+	/**
+	 * takes in basic information about the component
+	 * @param {String} rootSelector 
+	 */
+	constructor(rootSelector) {
+		this.rootSelector = rootSelector;
+	}
+
+	/**
+	 * inits the component
+	 */
+	init() {
+		this.root = document.querySelector(this.rootSelector);
+	}
+
+	/**
+	 * 
+	 * @param {Node} node 
+	 * @param {String} event 
+	 * @param {Function} listener 
+	 */
+	addListener(selector, event, listener) {
+		let node = document.querySelector(selector);
+		node.addEventListener(event, listener);
+	}
+}
+
+class Container extends Component {
+	constructor(rootSelector) {
+		super(rootSelector);
+	}
+
+	handleSearchInput() {
+
+	}
+
+	showPokemon() {
+
+	}
+}
+
+class Search extends Container {
+	constructor(rootSelector) {
+		super(rootSelector);
+
+		//state
+		this.input = '';
+
+		//bind listeners
+		this.handleKeyPress = this.handleKeyPress.bind(this);
+
+		//add listeners
+		this.addListener('.pokedex__search', 'keyup', this.handleKeyPress);
+	}
+
+	handleKeyPress(event) {
+		this.input = event.target.value;
+
+		let code = event.keyCode;
+		if (code == RETURN_KEYCODE) {
+			super.showPokemon(this.input);
+		} else {
+			// update the main display panel
+			this.handleSearchInput(this.input);
+		}
+	}
+}
+
+const app = new App();
+app.start();
