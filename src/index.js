41d0fc94de131dc05f2baf802da4ec401b9626e7
unholydeath785 <ivansonofcoul@gmail.com>
2018-04-21 13:09:16 -0700

    completed

--- src/index.js
+++ src/index.js
@@ -38,6 +38,10 @@ class Component {
 		action = action.bind(context);
 		this.actions.set(key, action);
 	}
+
+	toElement() {
+		return '<div></div>';
+	}
 }
 
 class Container extends Component {
@@ -45,11 +49,20 @@ class Container extends Component {
 		super(rootSelector);
 
 		this.search = new Search('.pokedex__searchbar');
-		this.display = new Display('.pokedex__display');
+		this.display = new Display('.pokedex__pokemon_container');
+		this.autofill = new Autofill('.pokedex__autofill');
 
 		this.getPokemonNames = this.getPokemonNames.bind(this);
 
 		this.search.addAction('pokemon', this.display.displayPokemon, this.display);
+		this.search.addAction('update', this.autofill.update, this.autofill);
+		this.search.addAction('list', this.display.displayList, this.display);
+
+		this.display.addAction('list', this.autofill.toElement, this.autofill);
+		this.display.addAction('handlers', this.autofill.addListHandlers, this.autofill);
+
+		this.autofill.addAction('pokemon', this.search.displayPokemon, this.search);
+
 		this.requestHandler.makeRequest("GET", COUNT_URL, this.getPokemonNames);
 	}
 
@@ -58,8 +71,9 @@ class Container extends Component {
 		let url = API_URL +  'pokemon/?limit=' + pokemonCount;
 		this.requestHandler.makeRequest("GET", url, function handleNames(data) {
 			let pokemons = JSON.parse(data);
-			this.names = pokemons.results;
 			this.display.hasFetchedNames = true;
+			this.autofill.setNames(pokemons.results);
+			this.display.render();
 		}.bind(this));
 	}
 }
@@ -91,7 +105,10 @@ class Display extends Component {
 			this.root.innerHTML = this.currentPokemon.toElement();
 		} else {
 			if (this.hasFetchedNames) {
-				// display autofill
+				let toEle = this.actions.get('list');
+				this.root.innerHTML = toEle();
+				let action = this.actions.get('handlers');
+				action();
 			} else {
 				// display loader
 				this.root.innerHTML = this.loader.toElement();
@@ -104,6 +121,71 @@ class Display extends Component {
 		let pokemon = new Pokemon(data);
 		this.currentPokemon = pokemon;
 		this.render();
+	}
+
+	displayList() {
+		this.display = this.displayTypes.LIST;
+		this.render();
+	}
+}
+
+class Autofill extends Component {
+	constructor(rootSelector) {
+		super(rootSelector);
+		this.hasFetchedNames = false;
+	}
+
+	update(input) {
+		if (this.hasFetchedNames) {
+			this.filteredNames = this.names.filter((x) => {
+				return x.name.includes(input);
+			});
+		}
+	}
+
+	setNames(names) {
+		this.names = names;
+		this.hasFetchedNames = true;
+		this.filteredNames = names;
+	}
+
+	toElement() {
+		let list = this.listToElement();
+		return (
+			`<div class="pokedex__autofill">
+				<h1 class="pokedex__autofill-list-title">Pokémon</h1>
+				${list}
+			</div>`
+		);
+	}
+
+	listToElement() {
+		if (this.hasFetchedNames) {
+			let ele = '';
+			for (let i = 0; i < this.filteredNames.length; i++) {
+				ele += (
+					`<div class="pokedex__autofill-pokemon">
+						<h3 class="pokedex__autofill-pokemon-name">
+							${this.filteredNames[i].name}
+						</h3>
+					</div>`
+				);
+			}
+			return ele;
+		} else {
+			return '';
+		}
+	}
+
+	addListHandlers() {
+		let nodes = document.querySelectorAll('.pokedex__autofill-pokemon');
+		for (let i = 0; i < nodes.length; i++) {
+			nodes[i].addEventListener('click', function (event) {
+				let pokemon = event.target.textContent.trim().toLowerCase();
+				let action = this.actions.get('pokemon');
+				action(pokemon);
+			}.bind(this));
+		}
 	}
 }
 
@@ -141,14 +223,17 @@ class Search extends Component {
 		let code = event.keyCode;
 		if (code == RETURN_KEYCODE) {
 			// direct to pokemon
-			this.displayPokemon();
+			this.displayPokemon(this.input);
 		} else {
-			// similar results
+			let action = this.actions.get('update');
+			action(this.input);
+			let render = this.actions.get('list');
+			render();
 		}
 	}
 
-	displayPokemon() {
-		let url = API_URL + 'pokemon/' + this.input;
+	displayPokemon(input) {
+		let url = API_URL + 'pokemon/' + input;
 		this.requestHandler.makeRequest('GET', url, (data) => {
 			let pokemon = JSON.parse(data);
 			let action = this.actions.get('pokemon');
